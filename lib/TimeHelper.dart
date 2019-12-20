cdab1682a54084edc05c4128ed3aa8d5ada61c98
Angius <koumarin@gmail.com>
2019-12-20 03:23:23 +0100

    Better time formatting

--- lib/TimeHelper.dart
+++ lib/TimeHelper.dart
@@ -1,4 +1,9 @@
 String HoursMinutesSecondsFromSeconds(int seconds) {
   var dur = Duration(seconds: seconds);
-  return '${dur.inHours}:${dur.inMinutes.remainder(60)}:${dur.inSeconds.remainder(60)}';
+
+  var h = dur.inHours.toString().padLeft(2, '0');
+  var m = dur.inMinutes.remainder(60).toString().padLeft(2, '0');
+  var s = dur.inSeconds.remainder(60).toString().padLeft(2, '0');
+
+  return '${h}:${m}:${s}';
 }
